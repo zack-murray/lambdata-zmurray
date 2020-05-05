d8ec78a580b98611e533bf9a5f9e15713a038ae3
Zack Murray <zachery.murray@gmail.com>
2020-05-05 17:14:51 -0400

    Pep8 revisions

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,56 +1,66 @@
 import pandas as pd
 from pandas import DataFrame
 
-# Turn list into series then add it as a new column to the dataframe     
-# df = dataframe, l = list, col_name = column name 
+# Turn list into series then add it as a new column to the dataframe
+# df = dataframe, l = list, col_name = column name
+
+
 def flto_nc(df, l, col_name):
-    df[col_name] = l 
+    df[col_name] = l
 
 # Split date into multiple columns
-# X = dataframe, column_name = self explanatory 
+# X = dataframe, column_name = self explanatory
+
+
 def split_column_date(X, column_name):
     X[column_name] = pd.to_datetime(X[column_name])
-    X[column_name +'_year'] = X[column_name].dt.year
-    X[column_name +'_month'] = X[column_name].dt.month
-    X[column_name +'_day'] = X[column_name].dt.day
+    X[column_name + '_year'] = X[column_name].dt.year
+    X[column_name + '_month'] = X[column_name].dt.month
+    X[column_name + '_day'] = X[column_name].dt.day
     X[column_name] = X[column_name].dt.strftime('%Y-%m-%d')
     return X
-    
+
 # Convert state abbreviations to full names
 # FL -> Florida, etc.
+
+
 def add_state_names(my_df):
-"""
-Converts a dataframe with a column of state abbreviations, adding a 
-corresponding 
 
-Params: my_df a pandas.DataFrame with a column called "abbrev".
+    """
+    Converts a dataframe with a column of state abbreviations, adding a
+    corresponding
 
-Example: add_state_names(DataFrame({"abbrev":["CA", "CO", "CT", "DC", "TX"]})
+    Params: my_df a pandas.DataFrame with a column called "abbrev".
 
-Returns: a pandas.DataFrame with the original col as well as a "name" column.
-"""
+    Example: add_state_names(DataFrame({"abbrev":["CA", "CO", "CT",
+    "DC", "TX"]})
 
-new_frame = my_df.copy()
+    Returns: a pandas.DataFrame with the original col as well as a
+    "name" column.
+    """
 
-# need a list with abbrev/name mappings
-names_map = {"CA":"California", "CO":"Colorado",
- "CT":"Connecticut","DC":"District of Columbia"}
+    new_frame = my_df.copy()
 
-# create a new column which maps the existing column using our names map
+    # need a list with abbrev/name mappings
+    names_map = {"CA": "California", "CO": "Colorado",
+                 "CT": "Connecticut", "DC": "District of Columbia"}
 
-# create breakpoint 
-#breakpoint()
+    # create a new column which maps the existing column using our names map
 
-new_frame["name"] = new_frame["abbrev"].map(names_map)
+    # create breakpoint
+    # breakpoint()
 
-return new_frame
+    new_frame["name"] = new_frame["abbrev"].map(names_map)
+
+    return new_frame
 
 if __name__ == "__main__":
-    #only run following code when we run this script 
+    # only run following code when we run this script
     # from the command-line
-    # otherwise don't invoke this code(for example when importing from another file)
-df = DataFrame({"abbrev":["CA", "CO", "CT", "DC", "TX"]})
-print(df.head())
+    # otherwise don't invoke this code(for example when importing from another
+    # file)
+    df = DataFrame({"abbrev": ["CA", "CO", "CT", "DC", "TX"]})
+    print(df.head())
 
-df2 = add_state_names(df)
-print(df2.head())
+    df2 = add_state_names(df)
+    print(df2.head())
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,14 +1,15 @@
 import pandas as pd
 
-#import my mod
-#from my-mod import flto_nc and dato_cl
+# import my mod
+# from my-mod import flto_nc and dato_cl
 from my_lambdata.my_mod import flto_nc
 from my_lambdata.my_mod import split_column_date
 
 # Instantiate df to test mod.py
-df = pd.DataFrame({"Friends":['Gene, Aaron, Dom, Scott, Zack'], 
-"Times won setback":[3,1,2,0,5072],"First game won":[12-31-2012,2-13-2015,
-10-9-2008,5-6-2007,10-2-1991,]})
+df = pd.DataFrame({"Friends": ['Gene, Aaron, Dom, Scott, Zack'],
+                   "Times won setback": [3, 1, 2, 0, 5072],
+                   "First game won": [12-31-2012, 2-13-2015, 10-9-2008,
+                                      5-6-2007, 10-2-1991]})
 
 # List also to test mod.py
-l = [5,10,15,20,25]
+l = [5, 10, 15, 20, 25]
